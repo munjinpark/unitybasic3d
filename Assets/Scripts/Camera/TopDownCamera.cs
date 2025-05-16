6e87be74aa3deafeca7cd74600518a76975bac10
Din Park <ppmmjj83@gmail.com>
2025-05-16 21:45:08 +0900

    feat : TopDown카메라 스크립트 추가

---
+++ Assets/Scripts/Camera/TopDownCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TopDownCamera : MonoBehaviour
+{
+    public Transform target;
+    public float height = 10f;
+    public float distance = 10f;
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (target)
+        {
+            transform.position = new Vector3(target.position.x, target.position.y + height, target.position.z - distance);
+            transform.LookAt(target);
+        }
+    }
+}
